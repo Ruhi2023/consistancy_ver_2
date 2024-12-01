f5d46998de13b57a833fb9e4aef5bd104def7eca
Ruhi2023 <ruhi0jaidaka@gmail.com>
2024-12-01 16:22:54 +0530

    Commit  final

--- pages/User_profile.py
+++ pages/User_profile.py
@@ -43,11 +43,35 @@ sco_date_wise= sorted_test_df.groupby(["test_date"],as_index=False)["score"].sum
 start_date = sorted_test_df["test_date"].min()
 end_date = sorted_test_df["test_date"].max()
 inconsistant_days = (end_date - start_date).days - sco_date_wise[sco_date_wise["score"]!=0].count().shape[0]
-st.write(inconsistant_days)
-
+st.markdown(f"### Consistant for last {inconsistant_days} days")
 # check if the user was consistant for last 5 days in row 
-df_last_5 = pd.DataFrame({"dates": pd.date_range(start_date, end_date, freq='D')})
-print(df_last_5)
+df_last_5 = pd.DataFrame({"test_date": pd.date_range(dt.date.today() - dt.timedelta(days=5), dt.date.today(), freq='D')})
+df_last_5["test_date"] = df_last_5["test_date"].dt.date
+cer = []
+for i in df_last_5["test_date"]:
+    if i in sco_date_wise["test_date"]:
+        cer.append(sco_date_wise[sco_date_wise["test_date"] == i]["score"].values[0])
+    else:
+        cer.append(0)
+
+df3= pd.DataFrame({"test_date": df_last_5["test_date"], "score": cer})
+df3["score"] = df3["score"].fillna(0)
+df3["score"] = df3["score"].astype(int)
+past_5_consistant = df3[df3["score"] != 0].shape[0]
+if past_5_consistant == 5:
+    st.write("Consistant for last 5 days")
+    st.write("Removing panelity ...")
+    st.write("Your total consistancy score is: ")
+    score = sco_date_wise["score"].sum()
+    st.markdown(f"Your total consistancy score is: **{score}**")
+elif past_5_consistant < 5 and inconsistant_days >10:
+    st.write("Not consistant for last 5 days")
+    st.write("You were inconsistant for more than 10 days")
+    st.write("Your total will be panalized ")
+    score = sco_date_wise["score"].sum() - sco_date_wise[sco_date_wise["score"]!=0].count().shape[0]*50
+    st.markdown(f"Your total consistancy score is: **{score}**")
+
+
 
 
 # w-flow: tests_per week user 
@@ -93,7 +117,6 @@ Score_avg_per_week2 = tests_df.groupby(pd.Grouper(key='test_date', freq='1W'))["
 Score_avg_per_week = Score_avg_per_week2.reset_index()
 Score_avg_per_week["test_date"] = pd.to_datetime(Score_avg_per_week["test_date"])
 st.header("Score avg per week user")
-st.write(Score_avg_per_week)
 fig2 = plt.figure(figsize=(5, 5))
 ax= fig2.subplots()
 ax.bar(x=Score_avg_per_week["test_date"], height=Score_avg_per_week["score"])
@@ -114,7 +137,6 @@ sco_sum_by_day = test_df_copy.groupby(["test_date"])["score"].sum()
 sco_sum_by_day = sco_sum_by_day.reset_index()
 sco_sum_by_day["test_date"] = pd.to_datetime(sco_sum_by_day["test_date"])
 st.header("Score sum per day user")
-st.write(sco_sum_by_day)
 fig3 = plt.figure(figsize=(15, 5))
 ax= fig3.subplots()
 ax.bar(x=sco_sum_by_day["test_date"], height=sco_sum_by_day["score"])
@@ -136,15 +158,16 @@ plt_sk = tp_per_wk["topic_id"].unstack(fill_value=0)
 
 st.line_chart(plt_sk, x_label="Date", y_label="Number of Topics", use_container_width=True)
 
-st.write(sco_sum_by_day.tail())
 if sco_sum_by_day["score"].sum() > 0:
-    st.header("Your total consistancy score is: ")
-    st.write(sco_sum_by_day["score"].sum())
-else:
-    sc_0 =sco_sum_by_day[sco_sum_by_day["score"] == 0].count()
-    st.header("Your total consistancy score is: ")
-    st.write(sco_sum_by_day["score"].sum()-sc_0["score"])
-    st.write("to remove debuff you will have to be consistant for 7 days in a row")
+    st.header("Your total tests score is: ")
+    co = sco_sum_by_day["score"].sum()
+    st.markdown(f"your total test scores are: **{co}**")
+# else:
+#     sc_0 =sco_sum_by_day[sco_sum_by_day["score"] == 0].count()
+#     st.header("Your total consistancy score is: ")
+#     co =sco_sum_by_day["score"].sum()-sc_0["score"]
+#     st.markdown(f"Your total test scores are: **{co}**")
+#     st.write("to remove debuff you will have to be consistant for 7 days in a row")
 # // st.write(tests_df)
 # // st.write(topics_df)
 # // st.write(qs_df)
